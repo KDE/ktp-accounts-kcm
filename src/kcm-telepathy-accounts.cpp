a84dd37c2bd7ef4fff43f16b69877880550ce8a4
George Goldberg <grundleborg@googlemail.com>
2009-01-25 14:05:00 +0000

    Add compileable stub KCM implementation.
    svn path=/trunk/playground/network/telepathy-accounts-kcm/; revision=916540

---
+++ src/kcm-telepathy-accounts.cpp
@@ -0,0 +1,43 @@
+/*
+ * This file is part of telepathy-accounts-kcm
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "kcm-telepathy-accounts.h"
+
+#include <kgenericfactory.h>
+
+
+K_PLUGIN_FACTORY(KCMTelepathyAccountsFactory, registerPlugin<KCMTelepathyAccounts>();)
+K_EXPORT_PLUGIN(KCMTelepathyAccountsFactory("telepathy_accounts", "kcm_telepathy_accounts"))
+
+
+KCMTelepathyAccounts::KCMTelepathyAccounts(QWidget *parent, const QVariantList& args)
+ : KCModule(KCMTelepathyAccountsFactory::componentData(), parent, args)
+{
+    // TODO: Implement me!
+}
+
+KCMTelepathyAccounts::~KCMTelepathyAccounts()
+{
+    // TODO: Implement me!
+}
+
+
+#include "kcm-telepathy-accounts.moc"
+
