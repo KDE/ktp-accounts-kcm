41a12c41766970e52c7dcb660a68bb9b58225f7c
George Goldberg <grundleborg@googlemail.com>
2009-07-22 21:06:32 +0000

    Add a ParameterEditWidget class. This class will provide the autogenerated fallback parameter-editing widget for the addAccount assistant.
    svn path=/trunk/playground/network/telepathy-accounts-kcm/; revision=1001302

---
+++ src/parameter-edit-widget.cpp
@@ -0,0 +1,56 @@
+/*
+ * This file is part of telepathy-accounts-kcm
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "parameter-edit-widget.h"
+
+#include "ui_parameter-edit-widget.h"
+
+#include <KDebug>
+
+class ParameterEditWidget::Private
+{
+public:
+    Private()
+     : ui(0)
+    {
+        kDebug();
+    }
+
+    Ui::ParameterEditWidget *ui;
+};
+
+ParameterEditWidget::ParameterEditWidget(QWidget *parent)
+ : QWidget(parent),
+   d(new Private)
+{
+    kDebug();
+
+    // Set up the UI.
+    d->ui = new Ui::ParameterEditWidget;
+    d->ui->setupUi(this);
+}
+
+ParameterEditWidget::~ParameterEditWidget()
+{
+    kDebug();
+
+    delete d;
+}
+
