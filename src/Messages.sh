f19074c9530175c6e9ee204240250d4bddaf5a64
Dario Freddi <dario.freddi@collabora.co.uk>
2010-11-09 16:40:07 +0100

    Add Messages.sh file

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/kcm_telepathy_accounts.pot
+$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
+$XGETTEXT `find . -name "*.cpp"` -o $podir/kcm_telepathy_accounts.pot
+rm -f rc.cpp
