75bfc20db897de922c78c443898ad85bc42e74c8
David Edmundson <kde@davidedmundson.co.uk>
2011-01-08 23:54:46 +0000

    improved work on item delegate

--- src/accounts-list-delegate.cpp
+++ src/accounts-list-delegate.cpp
@@ -1,5 +1,7 @@
 #include "accounts-list-delegate.h"
 
+#include "accounts-list-model.h"
+
 #include <QApplication>
 #include <QPainter>
 
@@ -23,15 +25,29 @@ void AccountsListDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
 
     style->drawPrimitive(QStyle::PE_PanelItemViewItem, &option, painter, 0);
 
-    QRect innerRect = option.rect.adjusted(4,4,-4,-4); //add some padding
-    QRect iconRect(innerRect.topLeft(), option.decorationSize);
-
     QIcon icon = index.data(Qt::DecorationRole).value<QIcon>();
+    QString accountName = index.data(Qt::DisplayRole).toString();
+    QString connectionStatusString = index.data(AccountsListModel::ConnectionStateDisplayRole).toString();
+
+    QRect innerRect = option.rect.adjusted(4,4,-4,-4); //add some padding
+
+    QSize decorationSize = option.decorationSize;
+    QSize statusSize = style->itemTextRect(option.fontMetrics, option.rect, Qt::AlignCenter , false , connectionStatusString).size();
+
+    QRect iconRect(innerRect.topLeft(), decorationSize);
+    QRect statusRect(QPoint(innerRect.right() - statusSize.width(), innerRect.top()), innerRect.bottomRight());
+
+
+    QRect textRect(iconRect.topRight() + QPoint(5,0), statusRect.bottomLeft() - QPoint(5,0));
+
+
     painter->drawPixmap(iconRect, icon.pixmap(option.decorationSize));
+    painter->drawText(textRect, accountName);
 
-    QRect textRect(iconRect.topRight(), innerRect.bottomRight());
+    textRect.adjust(0,option.fontMetrics.height()+2,0,0);
+    painter->drawText(textRect, "Connection Error");
 
-    painter->drawText(textRect, index.data(Qt::DisplayRole).toString());
+    painter->drawText(statusRect, Qt::AlignCenter, connectionStatusString);
 }
 
 
