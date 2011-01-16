7da8872a3015c145c5477d5bdcfb794b370b2709
Thomas Richard <thomas9999@gmail.com>
2011-01-16 11:09:12 +0100

    Low bandwidth parameter got hidden

--- gabble/server-settings-widget.cpp
+++ gabble/server-settings-widget.cpp
@@ -54,7 +54,7 @@ ServerSettingsWidget::ServerSettingsWidget(ParameterEditModel *model,
     handleParameter("server", QVariant::String, d->ui->serverLineEdit, d->ui->serverLabel);
     handleParameter("port", QVariant::UInt, d->ui->portSpinBox, d->ui->portLabel);
     handleParameter("keepalive-interval", QVariant::UInt, d->ui->keepaliveIntervalSpinBox, 0);
-    handleParameter("low-bandwith", QVariant::Bool, d->ui->lowBandwidthCheckBox, 0);
+    handleParameter("low-bandwidth", QVariant::Bool, d->ui->lowBandwidthCheckBox, 0);
     handleParameter("require-encryption", QVariant::Bool, d->ui->requireEncryptionCheckBox, 0);
     handleParameter("ignore-ssl-errors", QVariant::Bool, d->ui->ignoreSslErrorsCheckBox, 0);
     handleParameter("old-ssl", QVariant::Bool, d->ui->oldSslCheckBox, 0);
