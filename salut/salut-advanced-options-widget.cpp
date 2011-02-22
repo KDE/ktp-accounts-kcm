92bdeb7a00e5c1ba33c48841ca619a3b06e765dc
Florian Reinhard <florian.reinhard@googlemail.com>
2011-02-22 13:27:55 +0100

    Add telepathy-salut configuration plugin.
    Reviewed-by: David Edmundson <kde@davidedmundson.co.uk>

---
+++ salut/salut-advanced-options-widget.cpp
@@ -0,0 +1,46 @@
+/*
+ * This file is part of telepathy-accounts-kcm-plugins
+ *
+ * Copyright (C) 2011 Florian Reinhard <florian.reinhard@googlemail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "salut-advanced-options-widget.h"
+
+#include <KCMTelepathyAccounts/ParameterEditModel>
+
+#include <KDebug>
+
+SalutAdvancedOptionsWidget::SalutAdvancedOptionsWidget(ParameterEditModel* model, QWidget* parent)
+    : AbstractAccountParametersWidget(model, parent)
+{
+    kDebug();
+
+    // Set up the UI.
+    m_ui = new Ui::SalutAdvancedOptionsWidget;
+    m_ui->setupUi(this);
+
+    handleParameter("published-name",  QVariant::String, m_ui->pubnameLineEdit, m_ui->pubnameLabel);
+    handleParameter("email", QVariant::String, m_ui->emailLineEdit, m_ui->emailLabel);
+    handleParameter("jid",   QVariant::String, m_ui->jidLineEdit,   m_ui->jidLabel);
+}
+
+SalutAdvancedOptionsWidget::~SalutAdvancedOptionsWidget()
+{
+    delete m_ui;
+}
+
+#include "salut-advanced-options-widget.moc"
