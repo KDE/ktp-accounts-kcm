5e414c4e9c0c6b5dd9a05ba8ce2266a716aabef7
Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
2011-12-27 17:39:49 +0100

    Add .profile for msn using gabble and empty config dialog
    FEATURE: 289154

---
+++ plugins/gabble/main-options-widget-msn.cpp
@@ -0,0 +1,51 @@
+/*
+ * This file is part of telepathy-accounts-kcm
+ *
+ * Copyright (C) 2011 Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "main-options-widget-msn.h"
+
+#include <KCMTelepathyAccounts/ParameterEditModel>
+
+
+MainOptionsWidgetMSN::MainOptionsWidgetMSN(ParameterEditModel *model,
+                                     QWidget *parent)
+ : AbstractAccountParametersWidget(model, parent)
+{
+    // Set up the UI.
+    m_ui = new Ui::MainOptionsWidgetMSN;
+    m_ui->setupUi(this);
+}
+
+MainOptionsWidgetMSN::~MainOptionsWidgetMSN()
+{
+    delete m_ui;
+}
+
+void MainOptionsWidgetMSN::submit()
+{
+    AbstractAccountParametersWidget::submit();
+}
+
+bool MainOptionsWidgetMSN::validateParameterValues()
+{
+    return true;
+}
+
+
+#include "main-options-widget-msn.moc"
