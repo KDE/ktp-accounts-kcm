0528aabf89a763eafb11f9587c86fe83057fe16d
Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
2012-01-02 12:05:46 +0100

    Update display name in Haze - Skype

--- plugins/haze/skype-main-options-widget.cpp
+++ plugins/haze/skype-main-options-widget.cpp
@@ -31,7 +31,7 @@ SkypeMainOptionsWidget::SkypeMainOptionsWidget(ParameterEditModel *model, QWidge
     m_ui = new Ui::SkypeMainOptionsWidget;
     m_ui->setupUi(this);
 
-    handleParameter(QLatin1String("account"), QVariant::String, m_ui->accountLineEdit, m_ui->accountLabel);
+    handleParameter(QLatin1String("account"), QVariant::String, m_ui->accountLineEdit, m_ui->accountLabel, true);
 
 #ifdef Q_WS_X11
     // get autocomplete choices for the accountname
@@ -53,6 +53,11 @@ SkypeMainOptionsWidget::SkypeMainOptionsWidget(ParameterEditModel *model, QWidge
 SkypeMainOptionsWidget::~SkypeMainOptionsWidget()
 {
     delete m_ui;
+}
+
+void SkypeMainOptionsWidget::updateDefaultDisplayName()
+{
+    setDefaultDisplayName(m_ui->accountLineEdit->text());
 }
 
 #include "skype-main-options-widget.moc"
