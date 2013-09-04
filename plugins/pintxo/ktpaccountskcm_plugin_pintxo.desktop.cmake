294720edb86ef48836cbfe02492e58f4d798ed0c
l10n daemon script <scripty@kde.org>
2013-09-04 04:04:55 +0000

    SVN_SILENT made messages (.desktop file)

--- plugins/pintxo/ktpaccountskcm_plugin_pintxo.desktop.cmake
+++ plugins/pintxo/ktpaccountskcm_plugin_pintxo.desktop.cmake
@@ -1,8 +1,22 @@
 [Desktop Entry]
 Encoding=UTF-8
 Comment=Custom account configuration user interface for accounts using the Pintxo connection manager.
+Comment[cs]=Vlastní uživatelské rozhraní nastavení účtů pro účty používající správce spojení Pintxo.
+Comment[es]=Interfaz de configuración de cuentas personalizadas para las cuentas que usen el gestor de conexiones Pintxo.
+Comment[nl]=Gebruikersinterface voor aangepaste accountconfiguratie voor accounts die de Pintxo verbindingenbeheerder gebruiken.
+Comment[pt]=Interface de utilizador de configuração de contas personalizadas com o gestor de ligações Pintxo.
+Comment[sv]=Användargränssnitt för anpassad kontoinställning av konton som använder anslutningshanteraren Pintxo.
+Comment[uk]=Інтерфейс користувача для нетипового налаштовування облікових записів, для роботи яких використовуються засоби керування з’єднанням Pintxo.
+Comment[x-test]=xxCustom account configuration user interface for accounts using the Pintxo connection manager.xx
 Icon=phone
 Name=Pintxo Account Configuration User Interface
+Name[cs]=Uživatelské rozhraní nastavení účtu Pintxo
+Name[es]=Interfaz de configuración de cuentas Pintxo
+Name[nl]=Gebruikersinterface voor Pintxo accountconfiguratie
+Name[pt]=Interface de Configuração de Contas do Pintxo
+Name[sv]=Användargränssnitt för inställning av Pintxo-konton
+Name[uk]=Інтерфейс користувача для налаштовування облікового запису Pintxo
+Name[x-test]=xxPintxo Account Configuration User Interfacexx
 Type=Service
 ServiceTypes=KTpAccountsKCM/AccountUiPlugin
 
