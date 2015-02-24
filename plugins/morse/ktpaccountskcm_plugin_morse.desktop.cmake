4c2ec23428d208e878c7b3fca13585f90c1c5b57
l10n daemon script <scripty@kde.org>
2015-02-24 11:58:54 +0000

    SVN_SILENT made messages (.desktop file)

--- plugins/morse/ktpaccountskcm_plugin_morse.desktop.cmake
+++ plugins/morse/ktpaccountskcm_plugin_morse.desktop.cmake
@@ -1,35 +1,6 @@
 [Desktop Entry]
 Encoding=UTF-8
 Comment=Custom account configuration user interface for accounts using the Morse connection manager.
-Comment[bs]=Korisnički interfejs konfiguratora prilagođenih korisničkih naloga koji koristi Morse menadžer konekcija.
-Comment[ca]=Interfície d'usuari de configuració de compte personalitzat per comptes que utilitzen el gestor de connexions Morse.
-Comment[cs]=Vlastní uživatelské rozhraní nastavení účtů pro účty používající správce spojení Morse.
-Comment[da]=Brugerflade til brugertilpasset kontokonfiguration til konti der bruger forbindelseshåndteringen Morse.
-Comment[de]=Eigene Benutzeroberfläche zum Einrichten von Zugängen, die die Morse-Verbindungsverwaltung verwenden.
-Comment[en_GB]=Custom account configuration user interface for accounts using the Morse connection manager.
-Comment[es]=Interfaz de configuración de cuentas personalizadas para las cuentas que usen el gestor de conexiones «Morse».
-Comment[et]=Morse ühendusehaldurit kasutavate kontode kohandatud konto seadistamise kasutajaliides
-Comment[fi]=Morse-yhteyshallintaa käyttävien tilien mukautettu hallintakäyttöliittymä.
-Comment[fr]=Interface utilisateur personnalisée pour la configuration de comptes en utilisant le gestionnaire de connexions « Morse ».
-Comment[hu]=Egyedi beállítófelület a Morse kapcsolatkezelőt használó azonosítókhoz.
-Comment[ia]=Interfacie de usator pro configuration de conto personalisate pro contos usante le gerente de connexion Morse.
-Comment[it]=Interfaccia utente personalizzata di configurazione account per account che usano il gestore di connessioni Morse.
-Comment[nb]=Brukerflate for å sette opp egendefinerte kontoer ved bruk av Morse tilkoblingsbehandler.
-Comment[nl]=Gebruikersinterface voor aangepaste accountconfiguratie voor accounts die de Morse verbindingenbeheerder gebruiken.
-Comment[pl]=Dostosowana nakładka graficzna do ustawiania kont dla kont wykorzystujących usługę połączeń Morse.
-Comment[pt]=Interface de utilizador de configuração de contas personalizadas com o gestor de ligações Morse.
-Comment[pt_BR]=Interface de configuração de contas personalizadas usando o gerenciador de conexões Morse.
-Comment[sk]=Vlastné používateľské rozhranie konfigurácie účtu pre účty používajúce správcu pripojení Morse.
-Comment[sl]=Uporabniški vmesnik za prilagoditev računov, ki uporabljajo upravljalnik povezav Morse.
-Comment[sr]=Корисничко сучеље за подешавање посебних налога под менаџером везе Морзе.
-Comment[sr@ijekavian]=Корисничко сучеље за подешавање посебних налога под менаџером везе Морзе.
-Comment[sr@ijekavianlatin]=Korisničko sučelje za podešavanje posebnih naloga pod menadžerom veze Morse.
-Comment[sr@latin]=Korisničko sučelje za podešavanje posebnih naloga pod menadžerom veze Morse.
-Comment[sv]=Användargränssnitt för anpassad kontoinställning av konton som använder anslutningshanteraren Morse.
-Comment[tr]=Morse bağlantı yöneticisi kullanan hesaplar için özel hesap yapılandırma kullanıcı arayüzü.
-Comment[uk]=Інтерфейс користувача для нетипового налаштовування облікового запису за допомогою керування з’єднаннями Morse.
-Comment[x-test]=xxCustom account configuration user interface for accounts using the Morse connection manager.xx
-Comment[zh_CN]=通过 Morse 连接管理器自定义账户的配置界面
 Type=Service
 ServiceTypes=KTpAccountsKCM/AccountUiPlugin
 
