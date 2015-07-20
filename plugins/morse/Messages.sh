6fc0aa56432c19f41544d715fc303331b6d57d4a
Martin Klapetek <mklapetek@kde.org>
2015-07-20 16:48:43 +0200

    Port and enable the morse plugin

--- plugins/morse/Messages.sh
+++ plugins/morse/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp"` -o $podir/kcmtelepathyaccounts_plugin_butterfly.pot
+$XGETTEXT `find . -name "*.cpp"` -o $podir/kcmtelepathyaccounts_plugin_morse.pot
 rm -f rc.cpp
